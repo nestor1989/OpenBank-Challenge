cf49fb550328fe569175c4c450dce5ea2c646656
Néstor del Río <ndelrio@emergencias.com.ar>
2022-05-31 10:23:11 -0300

    first commit

---
+++ app/src/main/java/com/idea3d/open_bank_challengue/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.idea3d.open_bank_challengue
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
